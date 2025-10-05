9c10dffc9b3dbed0a172ad4d5a918cd3ca3814f5
Akshat <akshatrastogi09@gmail.com>
2025-10-06 01:46:15 +0530

    Adding home page to the application

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates  =  Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"item_id" : id}
