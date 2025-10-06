859c86e6aee82b37cf4372738309bb8de228ea39
Akshat <akshatrastogi09@gmail.com>
2025-10-07 00:57:18 +0530

    Modified the main file

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+import os
 from pymongo import MongoClient
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates  =  Jinja2Templates(directory="templates")
 
-conn = MongoClient("mongodb+srv://akshatrastogi:Akr1729@cluster1.f2xk0vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+conn = MongoClient(os.getenv("MONGODB_URI"))
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
