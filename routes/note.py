5ec27a53d021ec8c3d79ffbf428376429218122f
Akshat <akshatrastogi09@gmail.com>
2025-10-17 14:34:14 +0530

    Completed the delete api

--- routes/note.py
+++ routes/note.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from models.note import Note
 from config.db import conn
 from schemas.note import noteEntity, notesEntity
+from bson import ObjectId
 
 
 noteRouter = APIRouter()
 templates  =  Jinja2Templates(directory="templates")
-
+collection = conn.notes.notes
 
 @noteRouter.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    docs = conn.notes.notes.find({})
+    docs = collection.find({})
     showDocs = []
     for doc in docs:
         showDocs.append({
@@ -28,5 +28,14 @@ async def add_note(request: Request):
     form = await request.form()
     formDict = dict(form)
     formDict["important"] = "true" if formDict.get("important") == "on" else "false"
-    note = conn.notes.notes.insert_one(formDict)
+    note = collection.insert_one(formDict)
     return {"msg": "Note added successfully"}
+
+@noteRouter.get("/delete/{note_id}")
+async def delete_note(note_id: str):
+    note = collection.delete_one({"_id": ObjectId(note_id)})
+    return RedirectResponse("/", status_code=303)
+
+@noteRouter.get("/edit/{note_id}")
+async def edit_note(request: Request, note_id: str):
+    pass
